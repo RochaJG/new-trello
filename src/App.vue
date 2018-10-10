caeda018e6b97e4a8055564d71a903205c2b5dce
Jordan <70rd23@gmail.com>
2018-10-10 10:04:16 -0300

    Craição da rota para os Quadros

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,39 @@
 <template>
-  <span class="wrapper">
-    <nav-bar>
-      <li class="side-nav-title side-nav-item">Menu</li>
-      <li class="side-nav-item">
-        <router-link class="side-nav-link" to="/">
-          <i class="mdi mdi-home"></i>
-          <span> Início </span>
-        </router-link>
-      </li>
-    </nav-bar>
+  <div>
+    <span class="wrapper">
+      <nav-bar>
+        <li class="side-nav-title side-nav-item">Menu</li>
+        <menu-link goto="/" title="Início" icon="home" />
+        <menu-link goto="/boards" title="Quadros" icon="developer-board" />
+      </nav-bar>
 
-    <main class="content-page">
-      <div class="content">
-        <header-vue/>
-        <div class="container-fluid">
-          <router-view></router-view>
+      <main class="content-page">
+        <div class="content">
+          <header-vue/>
+          <div class="container-fluid">
+            <router-view></router-view>
+          </div>
         </div>
-      </div>
-    </main>
-  </span>
+      </main>
+    </span>
+
+    <right-bar></right-bar>
+  </div>
 </template>
 
 <script>
 import NavBar from '@/components/layouts/NavBar'
+import RightBar from '@/components/layouts/RightBar'
 import HeaderVue from '@/components/layouts/HeaderVue'
-
-var scope = {}
+import MenuLink from '@/components/layouts/MenuLink'
 
 export default {
   name: 'app',
   components: {
     NavBar,
-    HeaderVue
-  }
+    RightBar,
+    HeaderVue,
+    MenuLink,
+  },
 }
-
-// -----------------TRELLO API-----------------------
-
-function onAuthorize() {
-    updateLoggedIn()
-
-    Trello.members.get("me", function (member) {
-        scopeTrelloUser = member
-
-        // Busca as boards do usuário
-        Trello.get("members/me/boards", function (boards) {
-            $.each(boards, function (ix, board) {
-                //Busca cards abertas de cada board do usuário
-                Trello.get(`boards/${board.shortLink}/cards/open`, function (cards) {
-                    var myCards = []
-                    $.each(cards, function (ix, card) {
-                        $.each(card.idMembers, function (ix, member) {
-                            if (member == scopeTrelloUser.id) {
-                                myCards.push(card)
-                            }
-                        })
-                    })
-                    board.cards = myCards
-                })
-            })
-            scopeTrelloBoards = boards
-            console.log("------FIM-----")
-            console.log(scopeTrelloBoards)
-        })
-    })
-
-}
-
-var updateLoggedIn = function () {
-    scopeTrelloIsLoggedIn = Trello.authorized()
-}
-
-// scopeTrelloLogout = function () {
-//     Trello.deauthorize()
-//     updateLoggedIn()
-// }
-
-Trello.authorize({
-    interactive: false,
-    success: onAuthorize
-})
-
-// scopeTrelloLogin = function () {
-//     Trello.authorize({
-//         type: "popup",
-//         success: onAuthorize
-//     })
-// }
-
-        // -----------------TRELLO API END-----------------------
 </script>
---
+++ src/components/layouts/MenuLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <li class="side-nav-item">
+        <router-link class="side-nav-link" :to="goto">
+            <i :class="'mdi mdi-' + icon"></i>
+            <span> {{title}} </span>
+        </router-link>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'MenuLink',
+    props: ['goto', 'title', 'icon']
+}
+</script>
