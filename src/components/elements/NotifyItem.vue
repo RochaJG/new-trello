b3aa48a2a148f39aa2f6a83ae8d3a67286c82db5
Jordan <70rd23@gmail.com>
2018-10-16 13:50:46 -0300

    Estrutura base para as notificações na barra superior

---
+++ src/components/elements/NotifyItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <a href="javascript:void(0);" class="dropdown-item notify-item">
+        <div :class="'notify-icon bg-' + infoToNotif.color">
+            <i :class="'mdi mdi-'+ infoToNotif.icon"></i>
+        </div>
+        <p class="notify-details">{{notif.memberCreator.fullName}}</p>
+        <p class="text-muted mb-0 user-msg">
+            <b>{{infoToNotif.message}}{{notif.data.card.name}}</b>
+            <br>
+            <small>há 5 minutos</small>
+        </p>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'NotifyItem',
+    props: {
+        notif: Object
+    },
+    data() {
+        return {
+            dictonary: [
+                {
+                    type: 'addAttachmentToCard',
+                    message: 'Anexou um arquivo ao cartão: ',
+                    color: 'info',
+                    icon: 'attachment'
+                }
+            ]
+        }
+    },
+    computed: {
+        infoToNotif() {
+            var resp = {
+                        color: 'primary',
+                        icon: 'alert-circle',
+                        message: 'Atividade no cartão: '
+                    }
+            for(var dict of this.dictonary) {
+                if(this.notif.type == dict.type) {
+                    resp = dict
+                }
+            }
+
+            return resp
+        }
+    }
+}
+</script>
--- src/components/layouts/HeaderVue.vue
+++ src/components/layouts/HeaderVue.vue
@@ -2,28 +2,59 @@
 	<div class="navbar-custom">
 	<ul class="list-unstyled topbar-right-menu float-right mb-0">
 		<li class="dropdown notification-list">
-		<a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown" href="index.html#" role="button" aria-haspopup="false"
-			aria-expanded="false">
-			<span class="account-user-avatar"> 
-				<img :src="trelloUser.avatarUrl + '/50.png'" alt="user-image" class="rounded-circle">
-			</span>
-			<span>
-				<span class="account-user-name">{{trelloUser.fullName}}</span>
-				<span class="account-position text-info">@{{trelloUser.username}}</span>
-			</span>
-		</a>
-		<div class="dropdown-menu dropdown-menu-right dropdown-menu-animated profile-dropdown ">
-			<!-- item-->
-			<div class=" dropdown-header noti-title">
-				<h6 class="text-overflow m-0">Bem Vindo <span class="text-info">@{{trelloUser.username}}</span>!</h6>
-			</div>
-
-			<!-- item-->
-			<a @click="trelloLogout()" href class="dropdown-item notify-item">
-				<i class="mdi mdi-logout"></i>
-				<span>Logout</span>
+			<a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href role="button" aria-haspopup="false" aria-expanded="false">
+				<i class="dripicons-bell noti-icon"></i>
+				<span class="noti-icon-badge" v-show="hadNotif"></span>
 			</a>
-		</div>
+			<div class="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
+
+				<!-- item-->
+				<div class="dropdown-item noti-title">
+					<h5 class="m-0">
+						<span class="float-right">
+							<a href="javascript: void(0);" class="text-dark">
+								<small>Limpar Tudo</small>
+							</a>
+						</span>
+						Notificações
+					</h5>
+				</div>
+
+				<div class="slimscroll">
+					<notify-item v-for="notif in userNotif" :key="notif.id" :notif="notif"/>
+				</div>
+
+				<!-- All-->
+				<a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
+					Ver tudo
+				</a>
+
+			</div>
+		</li>
+
+		<li class="dropdown notification-list">
+			<a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown" href="index.html#" role="button" aria-haspopup="false"
+				aria-expanded="false">
+				<span class="account-user-avatar"> 
+					<img :src="trelloUser.avatarUrl + '/50.png'" alt="user-image" class="rounded-circle">
+				</span>
+				<span>
+					<span class="account-user-name">{{trelloUser.fullName}}</span>
+					<span class="account-position text-info">@{{trelloUser.username}}</span>
+				</span>
+			</a>
+			<div class="dropdown-menu dropdown-menu-right dropdown-menu-animated profile-dropdown ">
+				<!-- item-->
+				<div class=" dropdown-header noti-title">
+					<h6 class="text-overflow m-0">Bem Vindo <span class="text-info">@{{trelloUser.username}}</span>!</h6>
+				</div>
+
+				<!-- item-->
+				<a @click="trelloLogout()" href class="dropdown-item notify-item">
+					<i class="mdi mdi-logout"></i>
+					<span>Logout</span>
+				</a>
+			</div>
 		</li>
 	</ul>
 	<button class="button-menu-mobile open-left disable-btn">
@@ -33,17 +64,25 @@
 </template>
 
 <script>
+import NotifyItem from '@/components/elements/NotifyItem'
+
 export default {
 	name: 'HeaderVue',
+	components: {
+		NotifyItem
+	},
 	data() {
 		return {
-			trelloUser: {}
+			trelloUser: {},
+			userNotif: [],
+			hadNotif: false
 		}
 	},
 	methods: {
 		// Função inicializadora
 		loadTrelloInfo() {
 			this.getUserInfo()
+			this.trelloNotifications()
 		},
 		// Função para fazer a busca das informações do usuário
 		getUserInfo() {
@@ -77,6 +116,19 @@ export default {
 			Trello.deauthorize()
 			this.trelloStats = Trello.authorized()
 		},
+		trelloNotifications() {
+			var vm = this
+			Trello.get(`members/${this.trelloUser.id}/notifications`, notifications => {
+				var unreads = []
+				for(var notf of notifications) {
+					if(notf.unread) {
+						vm.hadNotif = true
+						unreads.push(notf)
+					}
+				}
+				vm.userNotif = unreads
+			})
+		},
 		// Função para atualizar a cada 3 segundos
 		autoUpdate() {			
 			this.updateDataInterval = setInterval(function () {
@@ -90,6 +142,14 @@ export default {
 			this.trelloLogin()
 		}
 		this.loadTrelloInfo()
+
+		$('.slimscroll').slimscroll({
+			height: '300px',
+			position: 'right',
+            size: '8px',
+            touchScrollStep: 20,
+            color: '#9ea5ab'
+		})
 	}
 }
 </script>
